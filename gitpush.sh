ab228605c1e5e3b35d405667e5d988fdada602b4
dingrui <dingrui198424@126.com>
2016-06-19 14:52:53 +0800

    update

--- gitpush.sh
+++ gitpush.sh
@@ -4,4 +4,5 @@ comment="$1"
 if [ ! "$comment" ]; then
     comment='update'
 fi
+find . -type d -empty -and -not -regex './.git.*' -exec touch {}/.gitignore \;
 git pull && git add -A && git commit -m "$comment" && git push
